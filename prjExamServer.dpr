e28f406334bd167c912c79f1ccf1ce1a2c27c21d
jiaping <jiaping1818@qq.com>
2015-09-20 20:45:42 +0800

    implemention Singleton

--- prjExamServer.dpr
+++ prjExamServer.dpr
@@ -1,61 +1,65 @@
 program prjExamServer;
-   {.$R uac.res uac.rc}
+
+{ .$R uac.res uac.rc }
 uses
 {$IFDEF DEBUG}
-  FastMM4,  {$ENDIF}
-  Forms,
-  uFormMainServer in 'Server\uFormMainServer.pas' {FormMainServer},
-  ExamineesManager in 'Server\ExamineesManager.pas',
-  ServerUtils in 'Server\ServerUtils.pas',
-  uDmServer in 'Server\uDmServer.pas' {dmServer: TDataModule},
-  uDataModulePool in 'Server\uDataModulePool.pas',
-  StkRecordInfo in 'Server\StkRecordInfo.pas',
-  ExamServer in 'ExamNet\ExamServer.pas',
-  frmExamineesImport in 'Server\frmExamineesImport.pas' {ExamineesImport},
-  ServerGlobal in 'Server\ServerGlobal.pas',
-  frmEnterForBaseImport in 'Server\frmEnterForBaseImport.pas' {EnterForBaseImport},
-  uTotalScoreForm in 'Server\uTotalScoreForm.pas' {TotalScoreForm},
-  SetExamPwd in 'Server\SetExamPwd.pas' {ResetExamPwdForm},
-  ufrmServerLogin in 'Server\ufrmServerLogin.pas' {FormServerLogin},
-  ufrmServerLock in 'Server\ufrmServerLock.pas' {FormServerLock};
+   FastMM4, {$ENDIF}
+   Forms, Winapi.Windows, uFormMainServer in 'Server\uFormMainServer.pas' {FormMainServer} , ExamineesManager in 'Server\ExamineesManager.pas',
+   ServerUtils in 'Server\ServerUtils.pas', uDmServer in 'Server\uDmServer.pas' {dmServer: TDataModule} , uDataModulePool in 'Server\uDataModulePool.pas',
+   StkRecordInfo in 'Server\StkRecordInfo.pas', ExamServer in 'ExamNet\ExamServer.pas',
+   frmExamineesImport in 'Server\frmExamineesImport.pas' {ExamineesImport} , ServerGlobal in 'Server\ServerGlobal.pas',
+   frmEnterForBaseImport in 'Server\frmEnterForBaseImport.pas' {EnterForBaseImport} , uTotalScoreForm in 'Server\uTotalScoreForm.pas' {TotalScoreForm} ,
+   SetExamPwd in 'Server\SetExamPwd.pas' {ResetExamPwdForm} , ufrmServerLogin in 'Server\ufrmServerLogin.pas' {FormServerLogin} ,
+   ufrmServerLock in 'Server\ufrmServerLock.pas' {FormServerLock};
 
 {$R *.res}
 
+var
+   mymutex: THandle;
 
 begin
-   {$IFDEF DEBUG}
+{$IFDEF DEBUG}
    ReportMemoryLeaksOnShutdown := True;
-   {$ENDIF}
-   // define REGISTER_EXPECTED_MEMORY_LEAK
-   // NeverSleepOnMMThreadContention := True;
+{$ENDIF}
+   mymutex := CreateMutex(nil, True, 'JPExamServer Singleton mutex object');
+   if GetLastError <> ERROR_ALREADY_EXISTS then
+   begin
 
-   Application.Initialize;
+      // define REGISTER_EXPECTED_MEMORY_LEAK
+      // NeverSleepOnMMThreadContention := True;
 
-   IsMultiThread                 := True;
-   Application.MainFormOnTaskbar := True;
-   // Application.CreateForm(TdmServer, GlobalDmServer);
-   Application.CreateForm(TFormMainServer, FormMainServer);
-  {$IFNDEF NOLOGIN  }
-   FormMainServer.visible  := false;
-   FormMainServer.Shadowed := false;
-   with TFormServerLogin.Create(Application) do
-      try
-         Shadowed := True;
-         if showModal = 1 then
-         begin
-            FormMainServer.visible  := True;
-            FormMainServer.Shadowed := True;
-            free;
-         end else begin
+      Application.Initialize;
+
+      IsMultiThread := True;
+      Application.MainFormOnTaskbar := True;
+      // Application.CreateForm(TdmServer, GlobalDmServer);
+      Application.CreateForm(TFormMainServer, FormMainServer);
+{$IFNDEF NOLOGIN  }
+      FormMainServer.visible := false;
+      FormMainServer.Shadowed := false;
+      with TFormServerLogin.Create(Application) do
+         try
+            Shadowed := True;
+            if showModal = 1 then
+            begin
+               FormMainServer.visible := True;
+               FormMainServer.Shadowed := True;
+               free;
+            end
+            else
+            begin
+               free;
+               Application.Terminate;
+            end;
+
+         except
             free;
             Application.Terminate;
          end;
-
-      except
-         free;
-         Application.Terminate;
-      end;
-   {$ENDIF}
-   Application.Run;
+{$ENDIF}
+      Application.Run;
+   end
+   else
+      Application.MessageBox('已经有一个实例在运行', '提示', mb_OK);
 
 end.
